a4ff41bfbaf04af65f651933b76796113ac449f2
antonio <antonio.popa.dev@gmail.com>
2025-05-29 12:16:50 +0300

    Added Service Provider controller

--- src/main/java/com/asusoftware/Skillio_Be/service_provider/controller/ServiceProviderController.java
+++ src/main/java/com/asusoftware/Skillio_Be/service_provider/controller/ServiceProviderController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/providers")
+@RequestMapping("/api/v1/providers")
 @RequiredArgsConstructor
 public class ServiceProviderController {
 
